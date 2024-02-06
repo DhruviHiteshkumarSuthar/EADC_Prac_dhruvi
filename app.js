45658f6b8d27d7e7bfa67fdce6aeb252c623ba43
Dhruvi Suthar <dhruvihsuthar21@gnu.ac.in>
2024-02-06 08:40:07 +0530

    Version 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello, this is a code by Dhruvi to test pipeline and this is the second version');
+    res.send('Hello ! I am Dhruvi and this is second version.');
   });
   const port = process.env.PORT || 3000;
 app.listen(port, () => {
